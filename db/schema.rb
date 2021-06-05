9c52d4b407ba05dd71e46d866239cd5989798e5c
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-05 16:26:17 +0900

    picturesテーブルのtitleが余分であったためtitleカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_033550) do
+ActiveRecord::Schema.define(version: 2021_06_05_072443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.bigint "user_id"
     t.datetime "created_at", null: false
