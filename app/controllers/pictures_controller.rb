14e86befc4b0db43d66a631f15aaa7a57b41eef2
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-05 13:44:07 +0900

    pictureのindexアクションを作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,6 @@
 class PicturesController < ApplicationController
+  # before_action :set_picture, only: [:show, :edit, :update, :destory]
+  def index
+    @pictures = Picture.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts do
-    collection do
-      post :confirm
-    end
-  end
+  # resources :posts do
+  #   collection do
+  #     post :confirm
+  #   end
+  # end
   resources :pictures do
     collection do
-      picture :confirm
+      post :confirm
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
